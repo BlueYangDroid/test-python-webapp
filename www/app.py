cdda7aa24d67cf6b0f0a8a9904211f0b4a2e126f
yanglijun1 <yanglijun1@hisense.com>
2018-03-21 18:15:13 +0800

    wrap web framework

--- www/app.py
+++ www/app.py
@@ -8,17 +8,150 @@ import logging; logging.basicConfig(level=logging.DEBUG)
 import asyncio, os, json, time
 from datetime import datetime
 from aiohttp import web
+from jinja2 import Environment, FileSystemLoader
 
-def index(request):
-	return web.Response(body=b'<h1>Test python</h1>', content_type='text/html')
+import orm
+from coroweb import add_routes, add_static
 
-async def init(loop):
-	app = web.Application(loop=loop)	
-	app.router.add_route('GET', '/', index)
-	srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
-	logging.info('server started at http://127.0.0.1:9000 ...')
-	return srv
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(init(loop))
-loop.run_forever()
+def init_jinja2(app, **kw):
+	logging.info('init jinja2...')
+	    # 配置options参数  
+	options = dict(  
+        # 自动转义xml/html的特殊字符  
+		autoescape = kw.get('autoescape', True),  
+        # 代码块的开始、结束标志  
+		block_start_string = kw.get('block_start_string', '{%'),  
+		block_end_string = kw.get('block_end_string', '%}'),  
+        # 变量的开始、结束标志  
+		variable_start_string = kw.get('variable_start_string', '{{'),  
+		variable_end_string = kw.get('variable_end_string', '}}'),  
+        # 自动加载修改后的模板文件  
+		auto_reload = kw.get('auto_reload', True)  
+    )      # 配置options参数  
+
+	# 获取模板文件夹路径 
+	path = kw.get('path', None)
+	if not path:
+		path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+
+	env = Environment(loader = FileSystemLoader(path), **options)
+	
+	# filters是Environment类的属性：过滤器处理字典
+	filters = kw.get('filters', None)
+	if filters:
+		for name, f in filters.items():
+			env.filters[name] = f
+
+	# 所有的模板和过滤器都给app全局保存
+	app['__template__'] = env
+
+
+# 编写用于输出日志的middleware
+# app, handler 由中间件框架传入:
+# 	handler是封装处理具体path视图函数的RequestHandler函数体
+# request是aiohttp传给path视图函数的RequestHandler的请求体
+async def logger_factory(app, handler):
+	async def logger(request):
+		logging.info('Request: %s %s' % (request.method, request.path))
+		return await handler(request)
+	return logger
+
+# 处理视图函数返回值，制作response的middleware，构造出真正的web.Response对象 
+async def response_factory(app, handler):
+	async def response(request):
+		logging.info('Response handler...')
+		# RequestHandler处理之后的切面
+		r = await handler(request)
+		logging.info('response result = %s' % str(r))
+		if isinstance(r, web.StreamResponse):
+			# StreamResponse是所有WebResponse的父类
+			return r
+		if isinstance(r, bytes):
+			resp = web.Response(body=r)
+			resp.content_type = 'application/octet-stream'
+			return resp
+		if isinstance(r, str):
+			if r.startswith('redirect:'): # 若返回重定向字符串 :
+				return web.HTTPFound(r[9:]) # 重定向至目标URL  
+			resp = web.Response(body=r.encode('utf-8'))
+			resp.content_type = 'text/html;charset=utf-8' # utf-8编码的text格式  
+			return resp
+
+		# r为dict对象时，调用内嵌的template路径
+		if isinstance(r, dict):
+			# 在后续构造视图函数返回值时，会加入__template__值，用以选择渲染的模板 
+			template = r.get('__template__', None)
+			if template is None: # 不带模板信息，返回json对象
+				# ensure_ascii：默认True，仅能输出ascii格式数据。故设置为False。  
+				# default：r对象会先被传入default中的函数进行处理，然后才被序列化为json对象  
+				# __dict__：以dict形式返回对象属性和值的映射  
+				resp = web.Response(body=json.dumps(r, ensure_ascii=False, default=lambda obj: obj.__dict__).encode('utf-8'))
+				resp.content_type = 'application/json;charset=utf-8'
+				return resp
+			else: # 带模板信息，渲染模板
+				# 调用Template对象的render()方法，传入r渲染模板，返回unicode格式字符串，将其用utf-8编码
+				resp = web.Response(body=app['__template__'].get_template(template).render(**r))
+				resp.content_type = 'text/html;charset=utf-8'
+				return resp
+
+		# 返回响应码
+		if isinstance(r, int) and (600>r>=100): 
+			resp = web.Response(status=r)
+			return resp
+
+		# 返回了一组响应代码和原因，如：(200, 'OK'), (404, 'Not Found')
+		if isinstance(r, tuple) and len(r) == 2:
+			status_code, message = r
+			if isinstance(status_code, int) and (600>status_code>=100):
+				resp = web.Response(status=r, text=str(message))
+
+		# 均以上条件不满足，默认返回
+		resp = web.Response(body=str(r).encode('utf-8'))
+		resp.content_type = 'text/plain;charset=utf-8'
+		return resp
+	return response
+
+# 赋给jinja时间类处理过滤器
+def datetime_filter(t):
+	delta = int(time.time() - t)
+	if delta < 60:
+		return u'1分钟前'
+	if delta < 3600:
+		return u'%s分钟前' % (delta//60)
+	if delta < 86400:
+		return u'%s小时前' % (delta//3600)
+	if delta < 604800:
+		return u'%s天前' % (delta//86400)
+	dt = datetime.fromtimestamp(t)
+	return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
+
+if __name__ == '__main__':
+
+	async def init(loop):
+		app = web.Application(loop = loop, middlewares=[logger_factory, response_factory])
+		init_jinja2(app, filters=dict(datetime = datetime_filter))
+		add_routes(app, 'test_view')
+		add_static(app)
+		srv = await loop.create_server(app.make_handler(), 'localhost', 9000)
+		logging.info('server started at http://127.0.0.1:9000...')
+		return srv
+
+	loop = asyncio.get_event_loop()
+	loop.run_until_complete(init(loop))
+	loop.run_forever()
+
+
+# def index(request):
+# 	return web.Response(body=b'<h1>Test python</h1>', content_type='text/html')
+
+# async def init(loop):
+# 	app = web.Application(loop=loop)	
+# 	app.router.add_route('GET', '/', index)
+# 	srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+# 	logging.info('server started at http://127.0.0.1:9000 ...')
+# 	return srv
+
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(init(loop))
+# loop.run_forever()
---
+++ www/coroweb.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import logging; logging.basicConfig(level=logging.DEBUG)
+import asyncio, os, inspect, functools
+from urllib import parse
+from aiohttp import web
+# from apis import APIError
+
+def Handler_decorator(path, *, method):
+	''' define decorator @get('/path') '''
+	def decorator(func):
+		@functools.wraps(func)
+		def wrapper(*args, **kw):
+			return func(*args, **kw)
+		wrapper.__method__ = method
+		wrapper.__route__ = path
+		return wrapper
+	return decorator
+
+get = functools.partial(Handler_decorator, method = 'GET')
+post = functools.partial(Handler_decorator, method = 'POST')
+
+# 使用inspect模块，检查视图函数的参数  
+  
+# inspect.Parameter.kind 类型：  
+# POSITIONAL_ONLY          位置参数  
+# KEYWORD_ONLY             命名关键词参数  
+# VAR_POSITIONAL           可选参数 *args  
+# VAR_KEYWORD              关键词参数 **kw  
+# POSITIONAL_OR_KEYWORD    位置或必选参数  
+def get_required_kw_args(fn):
+	args = []
+	params = inspect.signature(fn).parameters
+	for name, param in params.items():
+		if param.kind == inspect.Parameter.KEYWORD_ONLY and param.default == inspect.Parameter.empty:
+			args.append(name)
+	return tuple(args)
+
+def get_named_kw_args(fn):
+	args = []
+	params = inspect.signature(fn).parameters
+	for name, param in params.items():
+		if param.kind == inspect.Parameter.KEYWORD_ONLY:
+			args.append(name)
+	return tuple(args)
+
+def has_named_kw_args(fn):
+	params = inspect.signature(fn).parameters
+	for name, param in params.items():
+		if param.kind == inspect.Parameter.KEYWORD_ONLY:
+			return True
+
+def has_var_kw_arg(fn):
+	params = inspect.signature(fn).parameters
+	for name, param in params.items():
+		if param.kind == inspect.Parameter.VAR_KEYWORD:
+			return True
+
+def has_request_arg(fn):
+	sig = inspect.signature(fn)
+	params = sig.parameters
+	found = False
+	for name, param in params.items():
+		if name == 'request':
+			found = True
+			continue
+		if found and (param.kind != inspect.Parameter.VAR_POSITIONAL and param.kind != inspect.Parameter.KEYWORD_ONLY and param.kind != inspect.Parameter.VAR_KEYWORD):
+			raise ValueError('request parameter must be the last named parameter in function: %s%s' % (fn.__name__, str(sig)))
+	return found
+
+class RequestHandler(object):
+	"""docstring for RequestHandler"""
+	# 先构造fn进入
+	def __init__(self, app, fn):
+		super(RequestHandler, self).__init__()
+		self._app = app
+		self._func = fn
+		self._has_request_arg = has_request_arg(fn)
+		self._has_var_kw_arg = has_var_kw_arg(fn)
+		self._has_named_kw_args = has_named_kw_args(fn)
+		self._named_kw_args = get_named_kw_args(fn)
+		self._required_kw_args = get_required_kw_args(fn)
+
+	# 用预定的fn处理传入的request
+	async def __Call__(self, request):
+		kw = None
+
+		# 若视图函数有命名关键词或关键词参数 
+		if self._has_request_arg or self._has_named_kw_args or self._has_var_kw_arg:
+			if request.method == 'POST':
+				# json() body , post() form表单
+				if not request.content_type:
+					return web.HTTPBadRequest('Missing content_type.')
+				ct = request.content_type.lower()
+				if ct.startswith('application/json'):
+					params = await request.json
+					if not isinstance(params, dict):
+						return web.HTTPBadRequest('Json body must be object.')
+					kw = params
+				elif ct.startswith('applictaion/x-www-form-urlencoded') or ct.startswith('multipart/form-data'):
+					params = await request.post()
+					kw = dict(**params)
+				else:
+					return web.HTTPBadRequest('Unsupported content_type: %s ' % request.content_type)
+			
+			if request.method == 'GET': 
+				# 返回URL查询语句，?后的键值。string形式。
+				qs = request.query_string # 返回URL查询语句，?后的键值。string形式。  
+				if qs:  
+					kw = dict()  
+                    # '''''
+                    # 解析url中?后面的键值对的内容 
+                    # qs = 'first=f,s&second=s' 
+                    # parse.parse_qs(qs, True).items() 
+                    # >>> dict([('first', ['f,s']), ('second', ['s'])]) 
+                    # '''
+					for k, v in parse.parse_qs(qs, True).items():
+						# 返回查询变量和值的映射，True表示不忽略空格。
+						kw[k] = v[0] 
+
+		# 整理kw参数和request.match_info路径参数
+		if kw is None:  # 若request中无参数  
+			# 若存在可变路由：/a/{name}/c，可匹配path为：/a/jack/c的request 
+			# {variable}为参数名，传入request请求的path为值 
+			kw = dict(**request.match_info)
+		else:	# request有参数
+			if self._has_named_kw_args and (not self._has_var_kw_arg):
+				# 只有命名参数，没有关键字参数，需要只保留命名参数即可
+				copy = dict()
+				for name in self._named_kw_args:
+					if name in kw:
+						copy[name] = kw[name]
+				kw = copy
+
+			# 将request.match_info中的参数传入kw，但要覆盖kw中已有关键字参数
+			for k, v in request.match_info.items():
+				if k in kw:
+					logging.warn('Duplicat arg name in named arg and kw args: %s ' % k)
+				kw[k] = v
+
+		# 整理request参数
+		if self._has_request_arg:
+			kw['request'] = request
+
+		# 整理视图函数中存在无默认值的命名参数，检查是否已传入kw
+		if self._get_required_kw_args:
+			for name in self._required_kw_args:
+				if not name in kw:
+					return web.HTTPBadRequest('Missing argument: %s ' % name)
+
+		# 至此，kw为request带入给视图函数fn真正可调用的全部参数
+		logging.info('call with args: %s ' % str(kw))
+
+		try:
+			r = await self._func(**kw)
+			return r
+		except Exception as e:
+			return dict(error=e.error, data=e.data, message=e.message)
+
+# 添加静态文件，如image，css，javascript等
+def add_static(app):
+	# 拼接static文件目录
+	path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+	# path = os.path.join(os.path.abspath('.'), 'static')
+
+	app.router.add_static('/static/', path)
+	logging.info('add static %s => %s' % ('/static/', path))
+
+# 编写一个add_route函数，用来注册一个视图函数  
+def add_route(app, fn): 
+	method = getattr(fn, '__method__', None)
+	path = getattr(fn, '__route__', None)
+	if method is None or path is None:
+		raise ValueError('@get or @post not defined in %s.' % fn.__name__)
+	# 判断URL处理函数是否协程并且是生成器  
+	if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
+		fn = asyncio.coroutine(fn)
+	logging.info('add route %s %s => %s(%s)' % (method, path, fn.__name__, ','.join(inspect.signature(fn).parameters.keys())))  
+	app.router.add_route(method, path, RequestHandler(app, fn))
+
+
+# 导入模块，批量注册视图函数
+def add_routes(app, module_name):
+	n = module_name.rfind('.') # 从右侧检索，返回索引。若无，返回-1。
+	# 导入整个模块
+	if n == -1:
+		# __import__ 作用同import语句，但__import__是一个函数，并且只接收字符串作为参数
+		# __import__('os',globals(),locals(),['path','pip'], 0) ,等价于from os import path, pip
+		mod = __import__(module_name, globals(), locals())
+	else:
+		name = module_name[(n+1):]
+		# 导入所有的name模块的属性，为后续调用dir()
+		mod = getattr(__import__(module_name[:n], globals(), locals(), [name]), name)
+	for attr in dir(mod): # dir()迭代出mod模块中所有的类，实例及函数等对象,str形式
+		if attr.startswith('_'):
+			continue # 忽略'_'开头的对象，直接继续for循环
+		fn = getattr(mod, attr)
+		# 确保是函数
+		if callable(fn):
+			# 确保视图函数存在method和path
+			method = getattr(fn, '__method__', None)
+			path = getattr(fn, '__route__', None)
+			if method and path:
+				# 注册
+				add_route(app, fn)
